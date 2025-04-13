269859f3d81ab72f39b11a13653a9c15bb362e06
hemang-shimpi <hemangshimpi@outlook.com>
2025-04-12 20:30:44 -0400

    created product class for tech product

---
+++ Catalog/src/main/java/com/example/catalog/Product.java
@@ -0,0 +1,52 @@
+package com.example.catalog;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+
+    @Id
+    private Long id;
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    public Product() {}
+
+    public Product(String name, String description, BigDecimal price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    
+
+    
+}
